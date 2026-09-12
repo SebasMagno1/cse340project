ad97628a0e97f972bae0defb4c55c8f2e3d89fe9
SebasMagno1 <114171307+SebasMagno1@users.noreply.github.com>
2026-09-12 10:39:31 -0600

    commit 1a2b3c4d5e6f7g8h9i0j

--- server.js
+++ server.js
@@ -2,37 +2,38 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 // EJS configuration
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Serve static files from the public folder
+// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Home page
+// Home
 app.get("/", (req, res) => {
     res.render("index", {
         title: "Home"
     });
 });
 
-// Organizations page
+// Organizations
 app.get("/organizations", (req, res) => {
     res.render("organizations", {
         title: "Organizations"
     });
 });
 
-// Service projects page
+// Service Projects
 app.get("/projects", (req, res) => {
     res.render("projects", {
         title: "Service Projects"
     });
 });
 
-// Categories page
+// Categories
 app.get("/categories", (req, res) => {
     res.render("categories", {
         title: "Categories"
@@ -42,6 +43,13 @@ app.get("/categories", (req, res) => {
 // 404 page
 app.use((req, res) => {
     res.status(404).send("Sorry, page not found.");
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(500).send("Internal Server Error");
 });
 
 // Start server
